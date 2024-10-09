de6dc5a2710eebc586a7b42b32bff048849652c4
Max Radaev <m.radaev@geoscan.ru>
2024-10-09 13:13:51 +0300

    -add API

--- src/main/java/com/example/demo/service/ItemService.java
+++ src/main/java/com/example/demo/service/ItemService.java
@@ -4,6 +4,7 @@ package com.example.demo.service;
 import com.example.demo.DTO.ItemDto;
 import com.example.demo.model.Item;
 
+import java.awt.*;
 import java.util.List;
 import java.util.UUID;
 
@@ -13,4 +14,6 @@ public interface ItemService {
     List<Item> getListItems();
 
     boolean delete(UUID id);
+
+    List<Item> getListItemsByRadius(double radius, Point point);
 }
--- src/main/java/com/example/demo/service/ItemServiceImpl.java
+++ src/main/java/com/example/demo/service/ItemServiceImpl.java
@@ -3,8 +3,16 @@ package com.example.demo.service;
 import com.example.demo.DTO.ItemDto;
 import com.example.demo.model.Item;
 import com.example.demo.repository.ItemRepository;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Geometry;
+import org.locationtech.jts.geom.GeometryFactory;
 
-import java.util.*;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 
 @org.springframework.stereotype.Service
 public class ItemServiceImpl implements ItemService {
@@ -13,6 +21,24 @@ public class ItemServiceImpl implements ItemService {
 
     public ItemServiceImpl(ItemRepository itemRepository) {
         this.itemRepository = itemRepository;
+    }
+
+    @Override
+    public List<Item> getListItemsByRadius(double radius, Point point) {
+        GeometryFactory geometryFactory = new GeometryFactory();
+        org.locationtech.jts.geom.Point centerPoint = geometryFactory.createPoint(new Coordinate(point.x, point.y));
+        Geometry searchArea = centerPoint.buffer(radius);
+        List<Item> allItem = itemRepository.findAll();
+        List<Item> itemsInRadius = new ArrayList<>();
+        for (Item item : allItem) {
+            Coordinate itemCoordinate = new Coordinate(item.getLocation().getX(), item.getLocation().getY());
+            org.locationtech.jts.geom.Point itemPoint = geometryFactory.createPoint(itemCoordinate);
+
+            if (searchArea.contains(itemPoint)) {
+                itemsInRadius.add(item);
+            }
+        }
+        return itemsInRadius;
     }
 
 
