e8b9940f789f894d5afeb984501deb86866e98ee
Max Radaev <m.radaev@geoscan.ru>
2024-12-11 16:52:40 +0300

    TgBot: FIX

--- src/main/java/com/example/demo/bot/Bot.java
+++ src/main/java/com/example/demo/bot/Bot.java
@@ -2,6 +2,7 @@ package com.example.demo.bot;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
@@ -25,31 +26,29 @@ public class Bot implements LongPollingSingleThreadUpdateConsumer {
     @Override
     public void consume(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            if (update.getMessage().getText().equals("/start")) {
-                long chatId = update.getMessage().getChatId();
-                SendMessage message = SendMessage
-                        .builder()
-                        .chatId(chatId)
-                        .text("Привет!")
-                        .build();
-                try {
-                    telegramClient.execute(message);
-                } catch (TelegramApiException telegramApiException) {
-                    log.error(telegramApiException.getMessage());
-                }
-            }else {
-                long chatId = update.getMessage().getChatId();
-                SendMessage message = SendMessage
-                        .builder()
-                        .chatId(chatId)
-                        .text("Неизвестная команда")
-                        .build();
-                try {
-                    telegramClient.execute(message);
-                } catch (TelegramApiException telegramApiException) {
-                    log.error(telegramApiException.getMessage());
-                }
-            }
+            log.info(update.getMessage().getText());
+            handleUpdate(update);
+        }
+    }
+    private void handleUpdate(Update update) {
+        long chatId = update.getMessage().getChatId();
+        String responseText;
+
+        if (update.getMessage().getText().equals("/start")) {
+            responseText = "Привет!";
+        } else {
+            responseText = "Неизвестная команда";
+        }
+
+        SendMessage message = SendMessage.builder()
+                .chatId(chatId)
+                .text(responseText)
+                .build();
+
+        try {
+            telegramClient.execute(message);
+        } catch (TelegramApiException telegramApiException) {
+            log.error("Error sending message: {}", telegramApiException.getMessage());
         }
     }
 }
