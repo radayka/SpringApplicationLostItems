e0d8cd454bd556b30eb823e7afc10c3cda47edda
Max Radaev <m.radaev@geoscan.ru>
2024-12-11 16:29:48 +0300

    TgBot: FIX

--- src/main/java/com/example/demo/LostItemsApplication.java
+++ src/main/java/com/example/demo/LostItemsApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import com.example.demo.bot.TelegramBotRegister;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -8,6 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 public class LostItemsApplication {
     public static void main(String[] args) {
+        TelegramBotRegister register = new TelegramBotRegister();
+        register.startBot();
         SpringApplication.run(LostItemsApplication.class, args);
     }
 }
