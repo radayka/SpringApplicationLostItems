863fe3d117fe18b9083bf66a17d549687dbb2476
Max Radaev <m.radaev@geoscan.ru>
2024-12-12 17:49:05 +0300

    TgBot: add command

--- src/main/java/com/example/demo/bot/commands/AddUser.java
+++ src/main/java/com/example/demo/bot/commands/AddUser.java
@@ -3,11 +3,17 @@ package com.example.demo.bot.commands;
 import com.example.demo.bot.service.Command;
 import com.example.demo.entity.User;
 import com.example.demo.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
 
+@Slf4j
 public class AddUser implements Command {
     Update update;
     UserRepository userRepository;
+    TelegramClient telegramClient;
 
     @Override
     public void execute() {
@@ -16,5 +22,14 @@ public class AddUser implements Command {
         user.setLastName(update.getMessage().getFrom().getLastName());
         user.setUsername(update.getMessage().getFrom().getUserName());
         userRepository.save(user);
+        SendMessage sendMessage = SendMessage.builder()
+                .chatId(update.getMessage().getChatId())
+                .text("Пользователь добавлен")
+                .build();
+        try {
+            telegramClient.execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error(e.getMessage());
+        }
     }
 }
---
+++ src/main/java/com/example/demo/bot/commands/DeleteUser.java
@@ -0,0 +1,4 @@
+package com.example.demo.bot.commands;
+
+public class DeleteUser {
+}
